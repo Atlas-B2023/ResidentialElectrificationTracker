c641d920932a4a0ac8d72e8063083c2d6b1e6472
Sargeanthost <sargeanthost@gmail.com>
2023-11-24 18:35:36 -0500

    Deployed 1018d6a with MkDocs version: 1.5.3

--- gen_ref_pages.py
+++ gen_ref_pages.py
@@ -1,24 +1,40 @@
 """Generate the code reference pages."""
+# https://mkdocstrings.github.io/recipes/
 
 from pathlib import Path
-
 import mkdocs_gen_files
 
-#maybe take main out later when we have the gui, but for now its not needed.
+# maybe take main out later when we have the gui, but for now its not needed.
 exclude_words = ["__init__", "csv_merge", "main"]
+src_path = Path(__file__).parent.parent / "src"
 
-for path in sorted(Path("src").rglob("*.py")):
-    filename = path.with_suffix("")
-    rel_filename = path.relative_to("src").with_suffix("")
-    
-    filename_parts = filename.parts
+for path in sorted(src_path.rglob("*.py")):
+    module_path = path.relative_to(src_path).with_suffix("")
+    doc_path = path.relative_to(src_path).with_suffix(".md")
 
+    filename_parts = list(module_path.parts)
     if [elem for elem in exclude_words if elem in [part for part in filename_parts]]:
         continue
-    with mkdocs_gen_files.open(rel_filename.with_suffix(".md"), "w") as fd:
-        print("::: " + ".".join(filename_parts), file=fd)
 
-    mkdocs_gen_files.set_edit_path(rel_filename.with_suffix(".md"), "gen_ref_pages.py")
+    with mkdocs_gen_files.open(doc_path, "w") as fd:
+        identifier = ".".join(filename_parts)
+        print("::: " + identifier, file=fd)
+
+    mkdocs_gen_files.set_edit_path(doc_path, path)
+
+# for path in sorted(src_path.rglob("*.py")):
+#     filename = path.with_suffix("")
+#     rel_filename = path.relative_to("src").with_suffix("")
+
+#     filename_parts = filename.parts
+
+#     if [elem for elem in exclude_words if elem in [part for part in filename_parts]]:
+#         continue
+#     print(".".join(filename_parts))
+#     with mkdocs_gen_files.open(rel_filename.with_suffix(".md"), "w") as fd:
+#         print("::: " + ".".join(filename_parts), file=fd)
+
+#     mkdocs_gen_files.set_edit_path(rel_filename.with_suffix(".md"), "gen_ref_pages.py")
 
 
 # # src_folder = "src"
@@ -28,17 +44,17 @@ for path in sorted(Path("src").rglob("*.py")):
 
 # #     parts = py_filename.parts
 # #     #dont document the __init__ file in modules
-# #     if parts[-1] == "__init__":  
+# #     if parts[-1] == "__init__":
 # #         parts = parts[:-1]
 # #     elif parts[-1] == "__main__":
 # #         continue
 
-# #     with mkdocs_gen_files.open(md_filename, "w") as fd:  
+# #     with mkdocs_gen_files.open(md_filename, "w") as fd:
 # #         # get last part and change to py
-# #         print("::: " + src_folder + ".".join(parts), file=fd)  
-    
+# #         print("::: " + src_folder + ".".join(parts), file=fd)
+
 # #     mkdocs_gen_files.set_edit_path(md_filename, "gen_ref_pages.py")
-        
+
 # # for total in range(19, 100, 20):
 # #     filename = f"sample/{total}-bottles.md"
 
@@ -50,9 +66,9 @@ for path in sorted(Path("src").rglob("*.py")):
 # #     mkdocs_gen_files.set_edit_path(filename, "gen_ref_pages.py")
 
 
-# with mkdocs_gen_files.open("src/test/RedfinSearcher.md", "w") as fd:  
+# with mkdocs_gen_files.open("src/test/RedfinSearcher.md", "w") as fd:
 #     # get last part and change to py
-#     print("::: " + "src.test.RedfinSearcher", file=fd)  
+#     print("::: " + "src.test.RedfinSearcher", file=fd)
 #     # print("handler: python")
 #     # print("options:")
 #     # print("members:")
